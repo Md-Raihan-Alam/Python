a5d887e41a5972d7f7020772d30631048bd8d82f
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-10-25 21:51:40 +0600

    array

--- Array/Flipping-an-Image.py
+++ Array/Flipping-an-Image.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/flipping-an-image
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        output=list()
+        for i in image:
+            # print(i)
+            # i.reverse()
+            tempI=list()
+            for j in i[::-1]:
+                if j==0:
+                    tempI.append(1)
+                else:
+                    tempI.append(0)
+            output.append(tempI)
+        return output
